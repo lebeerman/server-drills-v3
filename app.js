af846c4fefdc6bbc5dfa2272855662d39a932f61
dan <omnidbeerman@gmail.com>
2017-11-14 12:23:48 -0700

    corrected status code assignments.

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(cors());
 app.set('port', (process.env.PORT || 5000));
 const data = [
   {
     id: 1,
-    firstName: "Alice",
-    lastName: "Zephyr",
-    homeTown: "Seattle"
+    firstName: 'Alice',
+    lastName: 'Zephyr',
+    homeTown: 'Seattle'
   },
   {
     id: 2,
-    firstName: "Bob",
-    lastName: "Yellow",
-    homeTown: "Vancouver"
+    firstName: 'Bob',
+    lastName: 'Yellow',
+    homeTown: 'Vancouver'
   },
   {
     id: 3,
-    firstName: "Claire",
-    lastName: "Xylitol",
-    homeTown: "Toledo"
+    firstName: 'Claire',
+    lastName: 'Xylitol',
+    homeTown: 'Toledo'
   },
   {
     id: 4,
-    firstName: "Daniel",
-    lastName: "Winston",
-    homeTown: "Akron"
+    firstName: 'Daniel',
+    lastName: 'Winston',
+    homeTown: 'Akron'
   },
   {
     id: 5,
-    firstName: "Edina",
-    lastName: "Veritas",
-    homeTown: "Wichita"
+    firstName: 'Edina',
+    lastName: 'Veritas',
+    homeTown: 'Wichita'
   }
-]
+];
 function getID(array, id) {
   for (var i = 0; i < array.length; i++) {
     if (array[i].id == id){
@@ -43,21 +43,21 @@ function getID(array, id) {
   }
   return false;
 }
-app.get("/", function(request, response){
+app.get('/', function(request, response){
   response.json(data);
 });
-app.get("/:id", function(request, response){
+app.get('/:id', function(request, response){
   if (getID(data,request.params.id)){
-  response.json(getID(data,request.params.id));
-  response.status = 200;
+    response.json(getID(data,request.params.id));
+    response.status(200);
   }
   else {
-    response.status = 404;
+    response.status(404);
     response.json({
-        error: {
-            message: "No record found!"
-        }
-      });
+      error: {
+        message: 'No record found!'
+      }
+    });
   }
 });
 app.listen(app.get('port'), function() {
